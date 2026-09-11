d927981c592336d0c3b0ee008c9811d0404ead5a
vrajkher <vrajkher@gmail.com>
2026-09-11 20:48:50 +0530

    Add package exports

---
+++ src/agent_librarian/__init__.py
@@ -0,0 +1,4 @@
+from .core.librarian import AgentLibrarian
+from .schemas.models import Task, Candidate, AgentKnowledgePacket
+
+__all__ = ["AgentLibrarian", "Task", "Candidate", "AgentKnowledgePacket"]
